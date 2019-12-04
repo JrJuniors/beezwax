bb71afde0ddba75078cceeb80330e21c019f6691
JrJuniors <58183217+JrJuniors@users.noreply.github.com>
2019-12-03 22:26:04 -0500

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,21 +1,44 @@
 <?php
 
-	$name = $_POST['name'];
-	$visitor_email = $_POST['email'];
-	$message = $_POST['message'];
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-    $email_from = 'beez.beezwax@gmail.com';
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
 
-    $email_subject = "New Beezwax Email";
+$email_from = 'beez.beezwax@gmail.com';
+$email_subject = "New Beezwax Email";
+$email_body = "You have received a new message from $name.\n".
+	      "Here is the message:\n $message".
 
-    $email_body = "You have received a new message from $name.\n".
-                  "Here is the message:\n $message".
-				  
-	$to = "beez.beezwax@gmail.com";
-	
-	$headers = "From: $email_from \r\n";
+$to = "beez.beezwax@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-	$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
 
-	mail($to,$email_subject,$email_body,$headers);
+header('Location: ThankYou.html');
+// Function to validate against any email injection attempts
+function IsInjected($str) {
+	$injections = array('(\n+)',
+		'(\r+)',
+		'(\t+)',
+		'(%0A+)',
+		'(%0D+)',
+		'(%08+)',
+		'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 ?>
