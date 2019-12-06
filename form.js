90638ee9087dfb74045fdeda625fa130b9cb66db
JrJuniors <58183217+JrJuniors@users.noreply.github.com>
2019-12-06 00:59:37 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,3 +1,6 @@
 function submit(){
-
+  var name = document.getElementById("name");
+  var email = document.getElemenyById("email");
+  var message = document.getElementById("message");
+  var select = document.getElementById("select");
 }
