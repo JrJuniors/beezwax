cf0edfdbe41a5386bf78db560dd3320c78693e48
JrJuniors <58183217+JrJuniors@users.noreply.github.com>
2019-12-06 01:11:32 -0500

    Update form.js

--- form.js
+++ form.js
@@ -2,5 +2,13 @@ function submit(){
   var name = document.getElementById("name");
   var email = document.getElemenyById("email");
   var message = document.getElementById("message");
+  var radio = document.getElementById("radio");
   var select = document.getElementById("select");
-}
+  
+  if (name.value == "") {
+    alert("Name must be filled out!");
+    name.style.background-color = "BlanchedAlmond"
+    return false;
+  }
+  
+ }
