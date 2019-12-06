81354fce8c7df3a5301682f7201028bce5af62d8
JrJuniors <58183217+JrJuniors@users.noreply.github.com>
2019-12-06 10:03:31 -0500

    Update select.js

--- select.js
+++ select.js
@@ -1,3 +1,13 @@
+/*
+Dominic Lutz
+Joshua Peoples
+12/6/2019
+Professor Kinuthia
+ISYS 288 - 001
+
+JavaScript document that populates the select input element
+*/
+
 var values = [ "select", "honey", "bumble", "leafcutter/mason", "digger/carpenter", "mining"];
 var texts = ["Select Bee:", "Honey Bee", "Bumble Bee", "Leafcutter/Mason Bee", "Digger/Carpenter Bee", "Mining Bee"];
 
