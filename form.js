31a98c90a0ffe7b71f778539d5195139932bd0c4
JrJuniors <58183217+JrJuniors@users.noreply.github.com>
2019-12-06 02:56:14 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,18 +1,46 @@
-document.getElementById("btnSend").addEventListener("click", submit);
-
+document.getElementById("btn").addEventListener("click", submit);
 function submit(){
-  var name = document.getElementById("name");
-  var email = document.getElemenyById("email");
-  var message = document.getElementById("message");
-  var radio = document.getElementById("radio");
-  var select = document.getElementById("select");
-  
-  alert(name.value);
-  
-  if (name.value == "") {
-   // alert("Name must be filled out!");
-    name.style.background-color = "BlanchedAlmond"
-    return false;
-  }
-  
- }
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var message = document.getElementById("message").value;
+	var radios = document.getElementById("radios").value;
+	var selects = document.getElementById("selects").value;
+	  
+	if (name == ""){
+		alert("Name must be entered!");
+		document.getElementById("name").style.backgroundColor = "blanchedalmond"; 
+		return false;
+	}
+	else{
+		document.getElementById("name").style.backgroundColor = "white"; 
+	}
+	
+	if (email == "" || email.startsWith("@") || email.endsWith("@") || !(email.includes("@"))){
+		alert("Valid email must be entered!");
+		document.getElementById("email").style.backgroundColor = "blanchedalmond"; 
+		return false;
+	}
+	else{
+		document.getElementById("email").style.backgroundColor = "white"; 
+	}
+ 
+	if (message == ""){
+		alert("Message must be entered!");
+		document.getElementById("message").style.backgroundColor = "blanchedalmond"; 
+		return false;
+	}
+	else{
+		document.getElementById("message").style.backgroundColor = "white"; 
+	}
+	
+	if (selects == "select"){
+		alert("Please pick your favorite Bee!");
+		document.getElementById("selects").style.backgroundColor = "blanchedalmond"; 
+		return false;
+	}
+	else{
+		document.getElementById("selects").style.backgroundColor = "white"; 
+	}
+	window.open("mailto:beez.beezwax@gmail.com?subject=Reply%20To%20" + email + "&body=Feedback:%20" + radios + "%0d%0aFavorite Bee:%20" + selects + "%0d%0aMessage:%20" + message);
+}
+ 
