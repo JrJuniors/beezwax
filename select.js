12ce0aa73eeff16721ab2f52ffda780f2cdd1b17
JrJuniors <58183217+JrJuniors@users.noreply.github.com>
2019-12-06 03:59:19 -0500

    Update select.js

--- select.js
+++ select.js
@@ -1,5 +1,10 @@
-alert("woo");
-var x = document.getElementById("selects");
-var c = document.createElement("option");
-c.text = "Kiwi";
-x.options.add(c);
+var values = [ "select", "honey", "bumble", "leafcutter/mason", "digger/carpenter", "mining"];
+var texts = ["Select Bee:", "Honey Bee", "Bumble Bee", "Leafcutter/Mason Bee", "Digger/Carpenter Bee", "Mining Bee"];
+
+var beeList = document.getElementById("selects");
+var i;
+for(i = 0; i < values.length; i++) {
+	var bee = new Option(texts[i], i);
+	bee.value = values[i];
+	beeList.options.add(bee);
+}
